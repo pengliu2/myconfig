6d7982f7c49c164f122e2ba3e9471f4fa2f79795
Peng Liu <a22543@motorola.com>
2014-01-02 13:16:44 -0600

    cscope to handle cpp and java

--- code/handy_scripts/cscope_ufilegen.sh
+++ code/handy_scripts/cscope_ufilegen.sh
@@ -1,7 +1,11 @@
 LNX=$1
 cd /
-find  $LNX                                                                \
-        -name "*.[chxsS]" -print > ${LNX}/cscope.files
+find $LNX                                                                \
+    -name "*.[chxsS]" -print > ${LNX}/cscope.files
+find $LNX                                                                \
+    -name "*.cpp" -print >> ${LNX}/cscope.files
+find $LNX                                                                \
+    -name "*.java" -print >> ${LNX}/cscope.files
 
 cd ${LNX}
 cscope -b -q
