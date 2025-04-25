38a014b02057ba03a3f38d358415c857c5a6844b
Peng Liu <pengliu@nvidia.com>
2025-04-25 09:38:30 -0700

    Update slack_notify.sh with new webhook URL and improved message formatting

--- config/slack_notify.sh
+++ config/slack_notify.sh
@@ -8,10 +8,16 @@ CURRENT_DIR=$(pwd)
 
 # Build the message
 if [ $# -eq 0 ]; then
-    MESSAGE="Task is done, on\n\nHost: ${HOSTNAME}\nDirectory: ${CURRENT_DIR}"
+    MESSAGE="Task is done, on
+
+Host: ${HOSTNAME}
+Directory: ${CURRENT_DIR}"
 else
-    MESSAGE="$*\n\nHost: ${HOSTNAME}\nDirectory: ${CURRENT_DIR}"
+    MESSAGE="$*
+
+Host: ${HOSTNAME}
+Directory: ${CURRENT_DIR}"
 fi
 
 # Send the notification to Slack
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' -d "{\"text\":\"$MESSAGE\"}" https://hooks.slack.com/services/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
